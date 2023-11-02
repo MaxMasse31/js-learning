7176bbd6c8da57835843b8343bc5659af5435dd4
Max Masse <maxmasse199@gmail.com>
2023-11-02 16:29:16 -0400

    ajout de tous les éléments

--- pieces.js
+++ pieces.js
@@ -1,21 +1,38 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
+const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-const article = pieces[0];
+for (let i = 0; i < pieces.length; i++) {
 
-// Utilisation de templates JavaScript
-const template = document.createElement('template');
-template.innerHTML = `
-  <img src="${article.image}">
-  <h2>${article.nom}</h2>
-  <p>Prix: ${article.prix} €</p>
-  <p>${article.categorie ?? "(aucune catégorie)"}</p>
-  <p>${article.description ?? "(aucune description)"}</p>
-  <p>${article.disponibilite=true ? "En stock" : "Rupture de stock"} <p/>`;
+    const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
 
-// Sélectionnez la section une seule fois
-const sectionFiches = document.querySelector(".fiches");
-
-// Ajoutez le contenu du template au DOM
-sectionFiches.appendChild(template.content);
+ }
+ 
